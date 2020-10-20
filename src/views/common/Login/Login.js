8a9579909f01982d4f65d87627ebb8d34d958893
Jason Pham <htpham@deakin.edu.au>
2020-10-20 14:37:52 +1030

    Silent CSS errors 2

--- src/views/common/Login/Login.js
+++ src/views/common/Login/Login.js
@@ -197,10 +197,12 @@ export const Login = () => {
     <MuiThemeProvider theme={applicationTheme}>
       <Grid container direction="row" justify="space-around" alignItems="center" className={classes.wrapper}>
         <Grid 
-          container
+          container item
           direction="column"
           justify="center"
           alignItems="center"
+          xs={12}
+          sm={6}
           >
           <img src={AppBadge} alt="App badge"/>
           <Typography variant="h3" component="h1" style={{ fontWeight: "bold", color: "#ffffff" }}>
@@ -213,7 +215,12 @@ export const Login = () => {
 
         <Grid
           className={classes.loginBox}
-          container
+          xs={12}
+          sm={6}
+          // md={4}
+          // lg={3}
+          // xl={2}
+          container item
           direction="column"
           justify="center"
           alignItems="center"
